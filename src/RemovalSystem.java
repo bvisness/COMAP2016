87d052b05a044b2da369c7b55e39418687fc3c6f
Ben Visness <bvisness@gmail.com>
2016-01-29 14:10:15 -0600

    Add RemovalSystem class

---
+++ src/RemovalSystem.java
@@ -0,0 +1,22 @@
+
+/**
+ * This class describes a debris removal system. It is intended to function as
+ * a "struct", of sorts; merely a collection of relevant data. Hence, no
+ * annoying getter and setter methods.
+ */
+public class RemovalSystem {
+	
+	public int costUpFront;
+	public int costPerDeployment;
+	public int costOfOperation;
+	
+	public int costOfFailureOneTime;
+	public int costOfFailurePerDeployment;
+	
+	public double riskOfFailureOneTime;
+	public double riskOfFailurePerDeployment;
+	
+	public int debrisPerDeployment;
+	public double maxDeploymentsPerYear;
+
+}
