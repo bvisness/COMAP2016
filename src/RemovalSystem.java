867df377fdd3d21c04a5580c99b19fdcf7cb7ac6
Ben Visness <bvisness@gmail.com>
2016-01-29 15:11:50 -0600

    Tweak formatting RemovalSystem

--- src/RemovalSystem.java
+++ src/RemovalSystem.java
@@ -1,21 +1,21 @@
 
 /**
- * This class describes a debris removal system. It is intended to function as
- * a "struct", of sorts; merely a collection of relevant data. Hence, no
- * annoying getter and setter methods.
+ * This class describes a debris removal system. It is intended to function as a
+ * "struct", of sorts; merely a collection of relevant data. Hence, no annoying
+ * getter and setter methods.
  */
 public class RemovalSystem {
-	
+
 	public int costUpFront;
 	public int costPerDeployment;
 	public int costOfOperation;
-	
+
 	public int costOfFailureOneTime;
 	public int costOfFailurePerDeployment;
-	
+
 	public double riskOfFailureOneTime;
 	public double riskOfFailurePerDeployment;
-	
+
 	public int debrisPerDeployment;
 	public double maxDeploymentsPerYear;
 
