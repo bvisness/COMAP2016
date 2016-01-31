ce8c571c3bcc32f042b9752e8fea4ba7246485ee
unknown <phg49389@PC11438.bu.ac.bethel.edu>
2016-01-31 09:25:59 -0600

    Removed defunct CombinationGenerator now that the extensible DeliveryType is working, and updated TestDriver code to use the newer, better method.

--- src/org/comap47577/model/v2_0/TestDriver.java
+++ src/org/comap47577/model/v2_0/TestDriver.java
@@ -3,18 +3,9 @@ package org.comap47577.model.v2_0;
 public class TestDriver {
 	
 	public static void main(String[] args) {
-		CombinationGenerator generator = new CombinationGenerator();
 		
-		RemovalSystem a = generator.getObject(
-			CombinationGenerator.deploymentType.rocket,
-			CombinationGenerator.vehicleSize.large,
-			CombinationGenerator.deorbitMethod.tether
-		);
-		RemovalSystem b = generator.getObject(
-			CombinationGenerator.deploymentType.skyhook,
-			CombinationGenerator.vehicleSize.small,
-			CombinationGenerator.deorbitMethod.laser
-		);
+		RemovalSystem a = new ExpendableRocket(new LargeSatellite(new Tether())).getObject();
+		RemovalSystem b = new Skyhook(new SmallSatellite(new Laser())).getObject();
 		
 //		a.debrisPerDeployment = 4;
 //		a.maxDeploymentsPerYear = 5;
