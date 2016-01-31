199035d36100ad4e8c65e79310cb78463486896e
Ben Visness <bvisness@gmail.com>
2016-01-31 11:34:57 -0600

    Tweak Tether values

--- src/org/comap47577/model/v2_0/Tether.java
+++ src/org/comap47577/model/v2_0/Tether.java
@@ -2,7 +2,7 @@ package org.comap47577.model.v2_0;
 
 public class Tether extends DeorbitMethod {
 	public Tether(){
-		super.takeDownRate = 4;
+		super.takeDownRate = 6;
 		super.buildCost = .5;
 		super.riskOfFailure = .005;
 	}
